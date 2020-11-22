30405b6837353c56689f76b2dc3b7ee7a325f157
mj <ubot01@nate.com>
2020-11-22 18:12:16 +0900

    [profile설정] 스케줄 실행은 profile dev와 sche만 실행되게 분리 prod에서는 sche과 분리해서 실행될 수 있게 prod profile추

--- src/main/java/com/assignment/coupon/config/SchedulerConfig.java
+++ src/main/java/com/assignment/coupon/config/SchedulerConfig.java
@@ -2,6 +2,7 @@ package com.assignment.coupon.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
@@ -9,9 +10,9 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 
+@Profile({"dev","sche"})
 @Configuration
 @EnableScheduling
-@EnableAsync
 public class SchedulerConfig implements SchedulingConfigurer {
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
